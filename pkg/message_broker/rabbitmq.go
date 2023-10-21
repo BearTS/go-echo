8bcfd7cea88f629385e58c75b3c882a32e1d88b7
Anuj Parihar <anujparihar@yahoo.com>
2023-10-21 05:34:21 +0530

    fix: rabbitmq ack

--- pkg/message_broker/rabbitmq.go
+++ pkg/message_broker/rabbitmq.go
@@ -2,7 +2,6 @@ package messagebroker
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/BearTS/go-echo/pkg/logger"
@@ -56,46 +55,6 @@ func (rh *RabbitMQHelper) Publish(ctx context.Context, exchange, routingKey stri
 // Get Channel
 func (rh *RabbitMQHelper) GetChannel() *amqp.Channel {
 	return rh.ch
-}
-
-// Consume creates a consumer and starts consuming messages from RabbitMQ.
-func (rh *RabbitMQHelper) Consume(queue, consumerName string, autoAck bool, handlerFunc func([]byte) error) error {
-	rh.ch.Qos(
-		rh.numOfWorkers, // prefetch count
-		0,               // prefetch size
-		false,           // global
-	)
-
-	msgs, err := rh.ch.Consume(
-		queue,        // queue
-		consumerName, // consumer
-		autoAck,      // auto-ack
-		false,        // exclusive
-		false,        // no-local
-		false,        // no-wait
-		nil,          // args
-	)
-	if err != nil {
-		return err
-	}
-
-	rh.logger.Info("Consuming messages from queue '%s' with consumer '%s'", queue, consumerName)
-
-	for i := 0; i < rh.numOfWorkers; i++ {
-		go func(workerID int) {
-			for delivery := range msgs {
-				rh.logger.Info(fmt.Sprintf("Worker %d received a message", workerID))
-				if err := handlerFunc(delivery.Body); err != nil {
-					log.Printf("Error processing message: %v", err)
-					// You can handle errors here, e.g., send the message to a dead-letter queue
-				} else {
-					delivery.Ack(false)
-				}
-			}
-		}(i)
-	}
-
-	return nil
 }
 
 // Close closes the connection to RabbitMQ.
